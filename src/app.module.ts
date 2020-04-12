248966185491a706c65578220f784143cbe58fd3
hanxianjie100 <HANXIANJIE100@pingan.com.cn>
2020-04-13 01:12:17 +0800

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Connection } from 'typeorm';
+import { User } from './user/user.entity';
+import {UsersModule} from './user/users.module'
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'Qwert123456&',
+    database: 'user_group',
+    entities: [User],
+    synchronize: true,
+  }), UsersModule],
+})
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
---
+++ src/user/users.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller()
+export class UsersController {
+  constructor(private readonly appService: UsersService) {}
+
+  @Get()
+  getHello() {
+  }
+}
